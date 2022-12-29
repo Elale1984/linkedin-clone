f7dff865e3ec42cfe0872571f8598d06f56251f0
Edward Lale <edward.gratefuldead1@gmail.com>
2022-12-28 22:04:54 -0600

    Added flip move animation

--- src/Feed.js
+++ src/Feed.js
@@ -1,80 +1,91 @@
-import React, { useEffect, useState } from 'react';
-import './Feed.css';
-import InputOption from './InputOption';
-import CreateIcon from '@mui/icons-material/Create';
-import ImageIcon from '@mui/icons-material/Image';
-import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
-import EventNoteIcon from '@mui/icons-material/EventNote';
-import CalendarViewDayIcon from '@mui/icons-material/CalendarViewDay';
-import Post from './Post';
+import React, { useEffect, useState } from "react";
+import "./Feed.css";
+import InputOption from "./InputOption";
+import CreateIcon from "@mui/icons-material/Create";
+import ImageIcon from "@mui/icons-material/Image";
+import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
+import EventNoteIcon from "@mui/icons-material/EventNote";
+import CalendarViewDayIcon from "@mui/icons-material/CalendarViewDay";
+import Post from "./Post";
 import { db } from "./firebase";
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
-import { useSelector } from 'react-redux';
-import { selectUser } from './features/userSlice'
-function Feed() {
-    const user = useSelector(selectUser)
-  const [input, setInput] = useState('');
-  const [posts, setPosts] = useState([])
-  
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+import { useSelector } from "react-redux";
+import { selectUser } from "./features/userSlice";
+import FlipMove from "react-flip-move";
 
-  useEffect(() => {
-    db.collection("posts").orderBy('timestamp', "desc").onSnapshot(snapshot => (
-        setPosts(snapshot.docs.map(doc => (
-            {
+function Feed() {
+    const user = useSelector(selectUser);
+    const [input, setInput] = useState("");
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        db.collection("posts")
+        .orderBy("timestamp", "desc")
+        .onSnapshot((snapshot) =>
+            setPosts(
+            snapshot.docs.map((doc) => ({
                 id: doc.id,
                 data: doc.data(),
-            }
-        )))
-    ))
-  }, []);
+            }))
+            )
+        );
+    }, []);
 
-  const sendPost = (e) => {
-    e.preventDefault();
-    db.collection('posts').add({
+    const sendPost = (e) => {
+        e.preventDefault();
+        db.collection("posts").add({
         name: user.displayName,
         description: user.email,
         message: input,
         photoUrl: user.photoUrl || "",
         timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-    });
+        });
 
-    setInput("");
-  };
+        setInput("");
+    };
 
-  return (
-    <div className="feed">
+    return (
+        <div className="feed">
         <div className="feed__inputContainer">
             <div className="feed__input">
-                <CreateIcon />
-                <form>
-                    <input value={input} onChange={e => setInput(e.target.value)} type="text" />
-                    <button onClick={sendPost} type='submit'>Send</button>
-                </form>
+            <CreateIcon />
+            <form>
+                <input
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                type="text"
+                />
+                <button onClick={sendPost} type="submit">
+                Send
+                </button>
+            </form>
             </div>
             <div className="feed__inputOptions">
-                <InputOption Icon={ImageIcon}title='Photo' color="#70B5F9"/>
-                <InputOption Icon={SubscriptionsIcon} title='Video' color="#E7A33E"/>
-                <InputOption Icon={EventNoteIcon} title='Event' color="#C0CBCD"/>
-                <InputOption Icon={CalendarViewDayIcon} title='Write article' color="#7FC15E"/>
+            <InputOption Icon={ImageIcon} title="Photo" color="#70B5F9" />
+            <InputOption Icon={SubscriptionsIcon} title="Video" color="#E7A33E" />
+            <InputOption Icon={EventNoteIcon} title="Event" color="#C0CBCD" />
+            <InputOption
+                Icon={CalendarViewDayIcon}
+                title="Write article"
+                color="#7FC15E"
+            />
             </div>
         </div>
-        
-        {posts.map(({id, data: { name, description, message,
-            photoUrl } }) => (
-            <Post 
+        <FlipMove>
+            {posts.map(({ id, data: { name, description, message, photoUrl } }) => (
+            <Post
                 key={id}
                 name={name}
                 description={description}
                 message={message}
                 photoUrl={photoUrl}
             />
-        ))}
-
-
-    </div>
-  );
+            ))}
+        </FlipMove>
+        </div>
+    );
 }
 
-export default Feed
+export default Feed;
